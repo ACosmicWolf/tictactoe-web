e3a5a04ee7b2737858b1bb2a02ce7cedc3b24f9f
ACosmicWolf <cosmicwolf009@gmail.com>
2023-03-04 22:11:30 +0530

    line drawing when win

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const getBlockMiddle = (col) => {
+  return 40 + (80 + 3) * col;
+};
+
 let game = [
   [0, 0, 0],
   [0, 0, 0],
@@ -7,6 +11,8 @@ let game = [
 let player = 1;
 
 let gameOver = false;
+
+const gameOverAudio = document.querySelector("#gameover");
 
 function setup() {
   document.querySelector("#game").classList.remove("over");
@@ -19,21 +25,53 @@ function setup() {
   ];
 
   // Reset cells
-  const cells = document.querySelectorAll(".cell");
-  cells.forEach((cell) => {
+  document.querySelectorAll(".cell").forEach((cell) => {
     cell.innerHTML = "";
     cell.classList.remove("occupied");
   });
 
-  // Reset winner
   document.querySelector(".winner").innerHTML = "";
   gameOver = false;
 
-  // Reset player
   player = 1;
-}
+  document.querySelectorAll(".line").forEach((line) => {
+    line.remove();
+  });
 
-const gameOverAudio = document.querySelector("#gameover");
+  document.querySelectorAll(".cell").forEach((cell) => {
+    cell.addEventListener("click", () => {
+      play(cell.dataset.row, cell.dataset.col);
+      if (document.querySelectorAll(".occupied").length === 9) {
+        checkWinner();
+        gameOver = true;
+        document.querySelector("#game").classList.add("over");
+        document.querySelector(".winner").innerHTML = "DRAW";
+        document.querySelector("#draw").play();
+        gameOverAudio.play();
+      }
+    });
+  });
+
+  /*  checkWinner();
+
+  if (!gameOver) {
+    // Reset winner
+    document.querySelector(".winner").innerHTML = "";
+    gameOver = false;
+  }
+
+  document.querySelectorAll(".row").forEach((row, rowIndx) => {
+    row.querySelectorAll(".cell").forEach((cell, colIndx) => {
+      cell.innerHTML =
+        game[rowIndx][colIndx] === 0
+          ? ""
+          : game[rowIndx][colIndx] === 1
+          ? "X"
+          : "O";
+      cell.classList.remove("occupied");
+    });
+  }); */
+}
 
 function checkWinner() {
   game.forEach((row) => {
@@ -55,6 +93,18 @@ function checkWinner() {
       gameOver = true;
       document.querySelector("#game").classList.add("over");
       document.querySelector(".winner").innerHTML = "Player 2 won";
+    }
+
+    if (player1Points === 3 || player2Points === 3) {
+      game.map((row, rowIndx) => {
+        console.log(row);
+        if (
+          (row[0] === 1 && row[1] === 1 && row[2] === 1) ||
+          (row[0] === 2 && row[1] === 2 && row[2] === 2)
+        ) {
+          linedraw(6, getBlockMiddle(rowIndx), 240, getBlockMiddle(rowIndx));
+        }
+      });
     }
   });
 
@@ -78,6 +128,17 @@ function checkWinner() {
       document.querySelector("#game").classList.add("over");
       document.querySelector(".winner").innerHTML = "PLAYER 2 WON";
     }
+
+    if (player1Points === 3 || player2Points === 3) {
+      for (let i = 0; i < 3; i++) {
+        if (
+          (game[0][i] === 1 && game[1][i] === 1 && game[2][i] === 1) ||
+          (game[0][i] === 2 && game[1][i] === 2 && game[2][i] === 2)
+        ) {
+          linedraw(getBlockMiddle(i), 6, getBlockMiddle(i), 240);
+        }
+      }
+    }
   }
 
   // Diagonal
@@ -92,12 +153,14 @@ function checkWinner() {
     gameOver = true;
     document.querySelector("#game").classList.add("over");
     document.querySelector(".winner").innerHTML = "PLAYER 1 WON";
+    linedraw(6, 6, 240, 240);
   }
 
   if (player2Points === 3) {
     gameOver = true;
     document.querySelector("#game").classList.add("over");
     document.querySelector(".winner").innerHTML = "PLAYER 2 WON";
+    linedraw(6, 6, 240, 240);
   }
 
   player1Points = 0;
@@ -114,12 +177,14 @@ function checkWinner() {
     gameOver = true;
     document.querySelector("#game").classList.add("over");
     document.querySelector(".winner").innerHTML = "PLAYER 1 WON";
+    linedraw(246, 0, 0, 246);
   }
 
   if (player2Points === 3) {
     gameOver = true;
     document.querySelector("#game").classList.add("over");
     document.querySelector(".winner").innerHTML = "PLAYER 2 WON";
+    linedraw(246, 0, 0, 246);
   }
 
   if (gameOver) {
@@ -144,20 +209,36 @@ function play(row, col) {
   checkWinner();
 }
 
+function linedraw(x1, y1, x2, y2) {
+  if (x2 < x1) {
+    var tmp;
+    tmp = x2;
+    x2 = x1;
+    x1 = tmp;
+    tmp = y2;
+    y2 = y1;
+    y1 = tmp;
+  }
+
+  var lineLength = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+  var m = (y2 - y1) / (x2 - x1);
+
+  var degree = (Math.atan(m) * 180) / Math.PI;
+
+  document.querySelector("#game").innerHTML +=
+    "<div class='line' style='transform-origin: top left; transform: rotate(" +
+    degree +
+    "deg); width: " +
+    lineLength +
+    "px; height: 4px; background: black; position: absolute; top: " +
+    y1 +
+    "px; left: " +
+    x1 +
+    "px;'></div>";
+}
+
+setup();
+
 document.querySelector("#reset").addEventListener("click", () => {
   setup();
-});
-
-document.querySelectorAll(".cell").forEach((cell) => {
-  cell.addEventListener("click", () => {
-    play(cell.dataset.row, cell.dataset.col);
-    if (document.querySelectorAll(".occupied").length === 9) {
-      checkWinner();
-      gameOver = true;
-      document.querySelector("#game").classList.add("over");
-      document.querySelector(".winner").innerHTML = "DRAW";
-      document.querySelector("#draw").play();
-      gameOverAudio.play();
-    }
-  });
 });
